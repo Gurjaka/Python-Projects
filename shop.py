daf927b277d3fa9c77a02496a8f0d793eb8b4b09
Gurjaka <esartia.gurika@gmail.com>
2024-05-25 20:51:46 +0400

    Shop update

--- shop.py
+++ shop.py
@@ -1,49 +1,91 @@
+# define currency
 CURRENCY = "GEL"
-
-goods = {
-    "1": {"Banana": 6},
-    "2": {"Apple": 5},
-    "3": {"Strawberry": 3.8},
-    "4": {"Peach": 4.3},
-    "5": {"Grapes": 32},
-    "6": {"Tomato": 1.4},
-    "7": {"Cucumber": 1.34},
-    "8": {"Pepper": 0.4},
+# Define menu options
+MENU = {
+    1: "Add item",
+    2: "List items",
+    3: "Total price",
+    4: "Modify item",
+    5: "Exit",
 }
+#define goods
+GOODS = {
+    "apple" :2,
+    "banana" : 3,
+    "orange" : 2.5,
+    "mustard" : 0.4,
+    "coke" : 2,
+    "milk" : 3,
+    "bread" : 1.5,
+    "cheese" : 15,
+    "mineral water" : 1.2,
+    "chocolate" : 6
+}
+basket = []
+# Function to display the menu
+def display_menu():
+    print("\n--- Shopping Basket ---")
+    for option, name in MENU.items():
+        print(f"{option}. {name}")
 
-cart = []
+def display_goods():
+    print("\n--- Available goods ---")
+    for name, price in GOODS.items():
+        print(f"{name} - {price} {CURRENCY}.")
+        
+def add_to_basket():
+    name = input("enter name: ").lower()
+    if name in GOODS:
+        quantity = int(input("enter quantity: "))
+        if basket !=[]:
+            for i in basket:
+                if i[0] == name:
+                    i[2] += quantity
+                else:
+                    basket.append([name, GOODS[name], quantity])
+        else:
+            basket.append([name, GOODS[name], quantity])
+        display_basket()
+    else:
+        print("this good is not in the shop")
+        
 
-price_list = 0
+def display_basket():
+    print("\n--- Your basket ---")
+    for item in basket:
+        print(f"{item[0]} - {item[2]} x {item[1]} {CURRENCY}")
 
-def menu():
-    for num, elem in goods.items():
-        for name, infos in elem.items():
-            print(f"{num}. {name} - {infos} {CURRENCY}")
 
-def shopping_cart():
-    global price_list
-    item = input("Choose your product using their index: ")
-    quantity = int(input("Choose quantity: "))
-    price = str(list(goods[item].keys())[0]) + ": " + str((list(goods[item].values())[0]) * quantity)
-    price_list_calc = (list(goods[item].values())[0]) * quantity
-    cart.append(price)
-    price_list = price_list + price_list_calc
+print(GOODS.items())
+
 
 while True:
-    menu()
-    shopping_cart()
-    exit = input("Have you finished shopping? (yes/no): ")
-    while exit.lower() not in ("yes", "no"):
-        exit = input("Have you finished shopping? (yes/no): ")
-    if exit.lower() in ("yes"):
-        print("You can now proceed to checkout!")
+    display_menu()
+    menu_choise = int(input("choise an option:"))
+    if menu_choise == 1:
+        display_goods()
+        add_to_basket()
+        
+    elif menu_choise == 2:
+        display_basket()
+    elif menu_choise == 3:
+        total = 0
+        for item in basket:
+            total += item[1] * int(item[2])
+        display_basket()    
+        print(f"Total price: {total} {CURRENCY}")
+    elif menu_choise == 4:
+        display_basket()
+        name = input("enter name: ").lower()
+        for item in basket:
+            if item[0] == name:
+                quantity = int(input("Choose quantity (0 to fully remove the item): "))
+                if quantity == 0:
+                    basket.remove(item)
+                    print("good removed")
+                else:
+                    item[2] = quantity
+                break
+        display_basket()
+    elif menu_choise == 5:
         break
-    elif exit.lower() in ("no"):
-        continue
-
-print("Here is your check:")
-
-for i in cart:
-    print(i)
-
-print(f"Your total is: {price_list}\n")
