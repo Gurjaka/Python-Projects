99c3e7543642041dbe16d54c4b246472b3238a47
Gurjaka <esartia.gurika@gmail.com>
2024-06-14 21:51:49 +0400

    Hangman update (bugs are still there)

--- hangman.py
+++ hangman.py
@@ -1,45 +1,98 @@
-# word = "apple"
-# word1 = ""
-
-
-# for i in word:
-#     if i == "p":
-#         i = "5"
-#     word1 += i
-
 import random
 
 words = ["happy", "hungry", "tired", "water", "book", "big", "hot", "cold", "open", "close"]
 
-w1 = "head"
-w2 = "body"
-w3 = "left hand"
-w4 = "right hand"
-w5 = "left foot"
-w6 = "right foot"
+warning = ["head", "body", "left hand", "right hand", "left foot", "right foot",]
+
+display = [
+"""
+     _____
+    |     |
+    |      
+    |
+    |
+    |
+""",
+"""
+     _____
+    |     |
+    |     O
+    |
+    |
+    |
+""",
+"""
+     _____
+    |     |
+    |     O
+    |     |
+    |
+    |
+""",
+"""
+     _____
+    |     |
+    |     O
+    |     |\\
+    |
+    |
+""",
+"""
+     _____
+    |     |
+    |     O
+    |    /|\\
+    |
+    |
+""",
+"""
+     _____
+    |     |
+    |     O
+    |    /|\\
+    |      \\
+    |
+""",
+"""
+     _____
+    |     |
+    |     O
+    |    /|\\
+    |    / \\
+    |
+"""]
 
 word_to_hang = random.choice(words)
 
 word_output = "_" * len(word_to_hang)
 
-guess_list = []
-
-attempts = 0
-while attempts < 6:
-    print("\n" + word_output, 6 - attempts, "attempts left.\n")
-    guess = input("Guess a letter: ")
+attempts = -1
+while True:
+    print(f"{display[attempts+1]}\n {word_output} {5 - attempts} attempts left.\n")
+    print(word_to_hang)
+    
     
     if word_output == word_to_hang:
         print("You WIN")
         break        
     
-    elif attempts == 6:
+    elif attempts == 5:
         print("You lose, you are out of attempts.")
+        break
+
+    guess = input("Guess a letter: ")
+
+
+    if guess not in word_to_hang:
+        attempts += 1
+        print(f"warning: {warning[attempts]}.")
+        continue
 
     else:
         for i in word_to_hang:
             if i == guess:
                 word_output = word_output[:word_to_hang.find(i)] + guess + word_output[word_to_hang.find(i)+1:]
-    attempts += 1
+        continue
 
-    
+
+
